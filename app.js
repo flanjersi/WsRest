56f4994fa484a4194310e3e99e08819fdf7d8682
flanjersi <grosj83670@gmail.com>
2018-12-19 12:15:47 +0100

    Add documentation to app.js

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ var http = require('http');
 
 const app = express()
 
+
+/**
+ * When user call the following url with the GET HTTP method : http://localhost:3000/api/posts
+ */
 app.get('/api/posts',  (req, res) => {
+
+    // Get the posts data from an external api
     http.get('http://jsonplaceholder.typicode.com/posts', (resp) => {
         let postsStr = '';
 
@@ -14,10 +20,12 @@ app.get('/api/posts',  (req, res) => {
         resp.on('end', () => {
             let posts = JSON.parse(postsStr);
 
+            // Add the date property to the data
             posts.forEach(function(post) {
                 post['date'] = Date.now();
             })
 
+            //To resolve an error
             res.header("Access-Control-Allow-Origin", "*");
             res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
@@ -29,6 +37,7 @@ app.get('/api/posts',  (req, res) => {
     });
 });
 
+//Listen on the port 3000
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 })
